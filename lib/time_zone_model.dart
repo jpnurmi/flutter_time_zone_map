55c300907687a040890fd0dedb6880caca1d9af1
J-P Nurmi <jpnurmi@gmail.com>
2021-08-01 18:14:36 +0200

    Model & service

--- lib/time_zone_model.dart
+++ lib/time_zone_model.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/foundation.dart';
 
 import 'time_zone_service.dart';
+import 'geo_location.dart';
 
 class TimeZoneModel extends ChangeNotifier {
   TimeZoneModel(this._service);
 
   final TimeZoneService _service;
+
+  String? lang;
+  String? release;
+
+  Future<Iterable<GeoLocation>> lookup(String query) {
+    return _service.lookupLocations(query, lang: lang, release: release);
+  }
 }
--- lib/time_zone_service.dart
+++ lib/time_zone_service.dart
@@ -1,7 +1,58 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import 'package:dio/dio.dart' as dio;
+
+import 'geo_location.dart';
+
+const _kBaseUrl = 'http://geoname-lookup.ubuntu.com/';
 
 class TimeZoneService {
-  const TimeZoneService(this._dio);
+  TimeZoneService(this._dio);
 
-  final Dio _dio;
+  final dio.Dio _dio;
+  dio.CancelToken? _token;
+
+  Future<Iterable<GeoLocation>> lookupLocations(
+    String query, {
+    String? release,
+    String? lang,
+  }) async {
+    return _cancelLookup()
+        .then((_) => _doLookup(query, release, lang))
+        .then((response) => _onLookupResponse(response))
+        .catchError((error) => _onLookupError(error));
+  }
+
+  Future<void> _cancelLookup() async => _token?.cancel();
+
+  static String _lookupUrl(String query, String? release, String? lang) {
+    return <String>[
+      _kBaseUrl,
+      '?query=$query',
+      if (release != null) '&release=$release',
+      if (lang != null) '&lang=$lang',
+    ].join();
+  }
+
+  Future<dio.Response> _doLookup(String query, String? release, String? lang) {
+    return _dio.get(
+      _lookupUrl(query, release, lang),
+      cancelToken: _token = dio.CancelToken(),
+      options: dio.Options(responseType: dio.ResponseType.plain),
+    );
+  }
+
+  Future<Iterable<GeoLocation>> _onLookupError(dio.DioError error) async {
+    if (dio.CancelToken.isCancel(error)) {
+      print('CANCEL: ${error.message}');
+    } else {
+      print('TODO: ${error.message}');
+    }
+    return <GeoLocation>[];
+  }
+
+  Future<Iterable<GeoLocation>> _onLookupResponse(dio.Response response) async {
+    final items = json.decode(response.data.toString()) as Iterable;
+    return items.map((json) => GeoLocation.fromJson(json));
+  }
 }
